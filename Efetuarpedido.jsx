d707936e8ba15b16cbd76e180bac6d516989ee5b
AHMAD08061994 <126988572+AHMAD08061994@users.noreply.github.com>
2023-10-20 16:10:03 -0300

    Add files via upload

---
+++ Efetuarpedido.jsx
@@ -0,0 +1,15 @@
+import Menu from '../componentes/Menu';
+import Footer from '../componentes/Footer'
+
+function EfetuarPedido(){
+
+    return (
+        <div>
+            <Menu/>
+            <Footer/>
+        </div>
+    )
+
+}
+
+export default EfetuarPedido
